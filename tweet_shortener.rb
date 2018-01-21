664302a7f0f16e1995ece5e157c51011d631cbac
Melissa Nock <melnock@gmail.com>
2018-01-21 23:40:42 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -36,7 +36,7 @@ end
 def shortened_tweet_truncator(tweet)
   if tweet.length > 140
     short_tweet = word_substituter(tweet)
-    if short_tweet > 140
+    if short_tweet.length > 140
       short_tweet = short_tweet.slice(0..137) + "..."
     else
       short_tweet
